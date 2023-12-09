b9ea20c944ae9db04798e6a7280b47dc5d256f06
DennohDev <dennismaina1024@gmail.com>
2023-12-10 00:10:36 +0300

    Completed Assignment

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
+# declarative_base() combines a container for table metadata as well as a group of methods that act as mappers between Python and our SQL database
+# Inheritance From Base allows us to avoid rewritting code
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db', echo=True)
+    Base.metadata.create_all(engine)
